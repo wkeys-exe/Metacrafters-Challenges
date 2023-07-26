5b9b9f8cdaee91531497b4470d22173a8f55e2d0
wkeys-exe <102484813+wkeys-exe@users.noreply.github.com>
2023-07-26 16:02:40 +0530

    Project

---
+++ ETH + AVAX PROOF Intermediate EVM Course/Module1/Project1/contract.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExceptionHandling {
+    function requireExample(uint256 x, uint256 y) public pure returns (uint256) {
+        // require() statement checks the condition and reverts if it evaluates to false
+        require(y != 0, "Division by zero is not allowed");
+        return x / y;
+    }
+
+    function assertExample(uint256 a, uint256 b) public pure returns (uint256) {
+        // assert() statement checks the condition and throws an error if it evaluates to false
+        assert(a >= b);
+        return a - b;
+    }
+
+    function revertExample(bool condition) public pure {
+        // revert() statement is used to revert the transaction with an error message
+        if (condition) {
+            revert("Custom error message");
+        }
+    }
+}
