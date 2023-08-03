8de203f0ea79461e716b44571c9a36ea78eae4a2
wkeys-exe <102484813+wkeys-exe@users.noreply.github.com>
2023-08-03 14:47:35 +0530

    Update contract.sol

--- ETH + AVAX PROOF Intermediate EVM Course/Module1/Project1/contract.sol
+++ ETH + AVAX PROOF Intermediate EVM Course/Module1/Project1/contract.sol
@@ -2,20 +2,17 @@
 pragma solidity ^0.8.10;
 
 contract ExceptionHandling {
-    function requireExample(uint256 x, uint256 y) public pure returns (uint256) {
-        // require() statement checks the condition and reverts if it evaluates to false
+    function requireStatement(uint256 x, uint256 y) public pure returns (uint256) {
         require(y != 0, "Division by zero is not allowed");
         return x / y;
     }
 
-    function assertExample(uint256 a, uint256 b) public pure returns (uint256) {
-        // assert() statement checks the condition and throws an error if it evaluates to false
+    function assertStatement(uint256 a, uint256 b) public pure returns (uint256) {
         assert(a >= b);
         return a - b;
     }
 
-    function revertExample(bool condition) public pure {
-        // revert() statement is used to revert the transaction with an error message
+    function revertStatement(bool condition) public pure {
         if (condition) {
             revert("error");
         }
