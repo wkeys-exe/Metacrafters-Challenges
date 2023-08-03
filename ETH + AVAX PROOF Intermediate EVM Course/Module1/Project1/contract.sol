9f08f2cba51a142b4c71efb9102c8237ada377ed
wkeys-exe <102484813+wkeys-exe@users.noreply.github.com>
2023-08-03 14:09:08 +0530

    Update contract.sol

--- ETH + AVAX PROOF Intermediate EVM Course/Module1/Project1/contract.sol
+++ ETH + AVAX PROOF Intermediate EVM Course/Module1/Project1/contract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 contract ExceptionHandling {
     function requireExample(uint256 x, uint256 y) public pure returns (uint256) {
